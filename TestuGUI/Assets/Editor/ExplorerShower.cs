8b5441ff72187f91cf1b94d4a1b9109bea8fea5d
榮勇樹 <sakae@prolead.co.jp>
2018-02-22 06:33:58 +0900

    unityproject追加

---
+++ TestuGUI/Assets/Editor/ExplorerShower.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class ExplorerShower
+{
+    static ExplorerShower()
+    {
+        EditorApplication.projectWindowItemOnGUI += OnProjectWindowItemOnGUI;
+    }
+
+    private static void OnProjectWindowItemOnGUI(string guid, Rect selectionRect) {
+        var rect = selectionRect;
+        rect.width = 20;
+        rect.x = selectionRect.xMax - 20;
+        if(GUI.Button(rect, "!"))
+        {
+            var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            assetPath = assetPath.Replace("/", "\\");
+            Process.Start("explorer.exe", "/select," + assetPath);
+        }
+    }
+}
